bed5e74b2b6dfd6623c8d0e1dbb87ceb8a9fec54
Joey Petrone <joey.petrone@gmail.com>
2020-04-11 23:59:35 -0500

    wip

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -8,6 +8,16 @@ const pinMaker = (pins) => {
   pins.forEach((pin) => {
     domString += '<div class="col-md-3 pl-2 pr-2 pb-4">';
     domString += `<div class="card" id="${pin.id}">`;
+
+    // domString += '<div class="btn-group">';
+    // domString += '<button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+    // domString += 'Move to';
+    // domString += '</button>';
+    // domString += '<div class="dropdown-menu">';
+    // domString += '<a class="dropdown-item" href="#"></a>';
+    // domString += '</div>';
+    // domString += '</div>';
+
     domString += `<img class="card-img" src="${pin.imageUrl}" alt=""/>`;
     domString += '<button class="btn btn-danger delete-pin">Delete</button>';
     domString += '</div>';
